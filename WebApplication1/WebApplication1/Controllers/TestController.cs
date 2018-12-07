998173f60d1b1196f9f18953b90898afc98ae83e
2686469571 <2686469571@qq.com>
2018-12-07 09:49:43 +0800

    ..

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+      public string GesString()
+        {
+            return "lalala";
+        }
+        public Customer getCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "abc";
+            c.Address = "def";
+            return c;
+        }
+        public ActionResult GetView()
+        {
+            return View("MyView");
+            
+        }
+    }
+}
